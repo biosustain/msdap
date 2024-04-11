8c19bbcdf40c7ada5bd911c43946bea271a6059f
Jakob Berg <JakobBerg@users.noreply.github.com>
2024-04-11 14:27:49 +0200

    into list

--- R/report_as_rmarkdown.R
+++ R/report_as_rmarkdown.R
@@ -178,9 +178,34 @@ print("try access with seq_along")
 
   print("plotly from contrast list")
   # Export each plotly object to Plotly JSON file
+  j = as.integer(0)
   for (contr in l_contrast) {
-    contrclass <- class(contr)
+    j= j+1
+    k = as.character(j)
+    contrclass <- class(contr) # list
     print(contrclass)
+      for (i in seq_along(contr)){
+        plot_gg <- contr[[i]]
+        plot_is_class <-class(plot_gg)
+        class(plot_gg)
+        print(plot_is_class) 
+        plot_x <- plot_gg$plot_env
+        plot_x_c <- class(plot_x)
+        print(plot_x_c)
+        plot_y <- plot_x$p
+        plot_y_c <- class(plot_y)
+        print(plot_y_c)
+        plot_plotly <- ggplotly(plot_y)
+        plotly_c <- class(plot_plotly)
+        print(plotly_c)
+        plot_json <- plotly_json(plot_plotly)
+        jplotly_c <- class(plot_json)
+        print(jplotly_c)
+        filename_json <- paste0(output_dir, "/plotb_", k, "_", i, ".json")
+        write(plot_json, file = filename_json)
+        filename_html <- paste0(output_dir, "/plotb_", k, "_", i, ".html")
+        htmlwidgets::saveWidget(plot_plotly, filename_html)
+      }
   }
 
   ################ history ################
