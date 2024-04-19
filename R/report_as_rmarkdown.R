1eed6d0aca37cad7c214369b2d7dbca45391c68b
Jakob Berg <JakobBerg@users.noreply.github.com>
2024-04-19 14:27:17 +0200

    define loop

--- R/report_as_rmarkdown.R
+++ R/report_as_rmarkdown.R
@@ -181,6 +181,7 @@ print("plot class for gg cscore")
         plot_is_class <-class(plot_list)
         class(plot_list)
         print(plot_is_class) 
+        if(testy_is_class == c("list")){
         for (m in seq_along(plot_list)){
           plot_gg <- plot_list[[m]]
           plot_is_class <-class(plot_gg)
@@ -198,27 +199,45 @@ print("plot class for gg cscore")
             filename_html <- paste0(output_dir, "/plotb_", k, "_", i,"_", m, ".html")
             htmlwidgets::saveWidget(plot_plotly, filename_html)
           }
-          # test plot separation
+          # test plot separation (sort before filter??)
           # try facet_null(shrink = TRUE) after filter
-          plot_ggg <- plot_gg
-          plot_ggg$data <- plot_ggg$data %>% filter(plottype == 'asis')
-          plot_gggg <- plot_ggg + facet_null(shrink = TRUE)
-          plot_is_class <-class(plot_gggg)
-          class(plot_gggg)
+            plot_ggg <- plot_gg
+            if exist / defined plot_ggg$data 
+            plot_ggg$data <- plot_ggg$data %>% filter(plottype == 'asis')
+            plot_gggg <- plot_ggg + facet_null(shrink = TRUE)
+            plot_is_class <-class(plot_gggg)
+            class(plot_gggg)
+            print(plot_is_class) 
+            if (isa(plot_gggg, c("gg","ggplot"))){
+              plot_plotly <- ggplotly(plot_gggg)
+              plotly_c <- class(plot_plotly)
+              print(plotly_c)
+              plot_json <- plotly_json(plot_plotly)
+              jplotly_c <- class(plot_json)
+              print(jplotly_c)
+              filename_json <- paste0(output_dir, "/plotbx_", k, "_", i,"_", m, ".json")
+              write(plot_json, file = filename_json)
+              filename_html <- paste0(output_dir, "/plotbx_", k, "_", i,"_", m, ".html")
+              htmlwidgets::saveWidget(plot_plotly, filename_html)
+            }
+          }
+        }
+          plot_gg <- testy
+          plot_is_class <-class(plot_gg)
+          class(plot_gg)
           print(plot_is_class) 
-          if (isa(plot_gggg, c("gg","ggplot"))){
-            plot_plotly <- ggplotly(plot_gggg)
+          if (isa(plot_gg, c("gg","ggplot"))){
+            plot_plotly <- ggplotly(plot_gg)
             plotly_c <- class(plot_plotly)
             print(plotly_c)
             plot_json <- plotly_json(plot_plotly)
             jplotly_c <- class(plot_json)
             print(jplotly_c)
-            filename_json <- paste0(output_dir, "/plotbx_", k, "_", i,"_", m, ".json")
+            filename_json <- paste0(output_dir, "/plotby_", k, "_", i,"_", m, ".json")
             write(plot_json, file = filename_json)
-            filename_html <- paste0(output_dir, "/plotbx_", k, "_", i,"_", m, ".html")
+            filename_html <- paste0(output_dir, "/plotby_", k, "_", i,"_", m, ".html")
             htmlwidgets::saveWidget(plot_plotly, filename_html)
           }
-        }
       }
   }
 
